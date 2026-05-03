2cf3683ee1123d80b361049a9a4a2da51b5d0659
Romain <rgeffrault@dood.com>
2026-05-03 06:36:58 +0200

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const { defineConfig } = require("eslint/config");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const craftRules = require("@craft-ng/dev-tools/eslint-rules");
+
+module.exports = defineConfig([
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    plugins: {
+      "craft-ng": craftRules,
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "craft-ng/brand-angular-gen-deps-required": "error",
+      "craft-ng/no-angular-inject": "error",
+      "craft-ng/prefer-craft-http-client": "error",
+      "craft-ng/prefer-craft-service": "error",
+      "craft-ng/prefer-browser-boundaries": "error",
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      angular.configs.templateRecommended,
+      angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+]);
