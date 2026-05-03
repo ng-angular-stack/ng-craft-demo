b556db26c8be901caf5df38280ee868397c39dfe
Romain <rgeffrault@dood.com>
2026-05-03 06:37:35 +0200

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const { defineConfig } = require("eslint/config");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
-const craftRules = require("@craft-ng/dev-tools/eslint-rules");
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const craftRules = require('@craft-ng/dev-tools/eslint-rules');
 
 module.exports = defineConfig([
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
       tseslint.configs.recommended,
@@ -15,39 +15,36 @@ module.exports = defineConfig([
       angular.configs.tsRecommended,
     ],
     plugins: {
-      "craft-ng": craftRules,
+      'craft-ng': craftRules,
     },
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
         },
       ],
-      "craft-ng/brand-angular-gen-deps-required": "error",
-      "craft-ng/no-angular-inject": "error",
-      "craft-ng/prefer-craft-http-client": "error",
-      "craft-ng/prefer-craft-service": "error",
-      "craft-ng/prefer-browser-boundaries": "error",
+      'craft-ng/brand-angular-gen-deps-required': 'error',
+      'craft-ng/no-angular-inject': 'error',
+      'craft-ng/prefer-craft-http-client': 'error',
+      'craft-ng/prefer-craft-service': 'error',
+      'craft-ng/prefer-browser-boundaries': 'error',
     },
   },
   {
-    files: ["**/*.html"],
-    extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
-    ],
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
     rules: {},
-  }
+  },
 ]);
